2cd1832718cc3fd59061d17f94ff1fb1e54bf0fe
Yuval Iacob <burpie@gmail.com>
2024-01-22 18:25:23 +0100

    More updates to Form and List

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,34 +2,44 @@ import "./Form.css";
 
 export default function Form({ onAddActivity }) {
   function handleAddActivity(event) {
+    // event.preventDefault();
+    // // function creates the data name and email from the form below
+    // const formData = new FormData(event.target);
+    // const data = Object.fromEntries(formData);
+    // const formInput = {
+    //   name: data.name.value,
+    //   isForGoodWeather: data.weather.checked,
+    // };
+
+    // console.log(formInput);
+
+    // // calls the prop-function from App.js and passes the created data name and email as arguments (lifts up)
+    // onAddActivity(formInput);
+
+    // event.target.reset();
+    // event.target.elements.name.focus();
+
     event.preventDefault();
-    // function creates the data name and email from the form below
     const formData = new FormData(event.target);
     const data = Object.fromEntries(formData);
-    const formInput = {
-      name: data.name.value,
-      isForGoodWeather: data.weather.checked,
-    };
 
-    // calls the prop-function from App.js and passes the created data name and email as arguments (lifts up)
-    onAddActivity(formInput);
+    onAddActivity(data);
+    console.log(data);
 
     event.target.reset();
     event.target.elements.name.focus();
   }
 
   return (
-    <form className="form">
-      <h2 className="entry-form__title">Add new activity:</h2>
-      <label htmlFor="motto">Name:</label>
+    <form onAddActivity={handleAddActivity}>
+      <h2>Add new activity:</h2>
+      <label htmlFor="name">Name:</label>
       <input type="text" name="name" id="name" />
 
-      <label htmlFor="notes">Good-weather activity:</label>
+      <label htmlFor="weather">Good-weather activity:</label>
       <input type="checkbox" name="weather" id="weather" />
 
-      <button type="submit" onAddActivity={handleAddActivity}>
-        Submit
-      </button>
+      <button type="submit">Submit</button>
     </form>
   );
 }
---
+++ src/components/List.jsx
@@ -0,0 +1,13 @@
+import "./List.css";
+
+export default function List({ activities }) {
+  return (
+    <ul className="list">
+      {activities.map((activity) => (
+        <li key={activity.id}>
+          <h3>{activity.name}</h3>
+        </li>
+      ))}
+    </ul>
+  );
+}
