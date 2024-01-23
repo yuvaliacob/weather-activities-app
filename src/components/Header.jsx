47bca8eaa64e9a258352acab838d9a97bc7532df
Tillmann Severin <tillmann.severin@posteo.de>
2024-01-23 18:08:22 +0100

    add arctic

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,21 @@
 import "./Header.css";
 
-export default function Header({ condition, temperature, headline }) {
+export default function Header({
+  condition,
+  temperature,
+  headline,
+  onChangeLocation,
+}) {
+  function handleClick() {
+    onChangeLocation("arctic");
+  }
   return (
     <>
       <h1>
         {condition} {temperature}
       </h1>
       <p>{headline}</p>
+      <button onClick={handleClick}>arctic</button>
     </>
   );
 }
