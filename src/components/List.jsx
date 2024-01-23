30a9c82fcc74c907f6c74980e343d2e9b01003bf
Yuval Iacob <burpie@gmail.com>
2024-01-23 10:11:50 +0100

    Add basic design

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -2,12 +2,15 @@ import "./List.css";
 
 export default function List({ activities }) {
   return (
-    <ul className="list">
-      {activities.map((activity) => (
-        <li key={activity.id}>
-          <h3>{activity.name}</h3>
-        </li>
-      ))}
-    </ul>
+    <>
+      <ul className="list">
+        {activities.map((activity) => (
+          <li key={activity.id}>
+            <h3>{activity.name}</h3>
+          </li>
+        ))}
+      </ul>
+      <div className="separator"></div>
+    </>
   );
 }
